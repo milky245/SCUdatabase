f73146a5bebb67041be4217341a0e2ad0e0c7b5e
MilkyCoco <68319358+milky245@users.noreply.github.com>
2021-10-08 13:29:20 +0800

    Add files via upload

--- q10_christmas.sql
+++ q10_christmas.sql
@@ -1,2 +1,7 @@
 /*Concatenate the ProductNames ordered by the Company 'Queen
 Cozinha' on 2014-12-25*/
+SELECT ProductName, P.Id, rank() OVER (ORDER BY P.Id) AS IdRank
+FROM Product P, OrderDetail, `Order` O, Customer
+WHERE P.Id=OrderDetail.ProductId AND OrderDetail.OrderId=O.Id AND O.CustomerId=Customer.Id 
+	AND Customer.CompanyName =  'Queen Cozinha' AND O.OrderDate LIKE '2014-12-25%'
+ORDER BY P.Id
